6483ccfbacaff3e5fef08e5269baa265f32bda64
harsh.tanti <harshtanti59@gmail.com>
2025-01-26 22:17:16 +0530

    folder structure setup

--- app/src/main/java/com/example/adsperclick/views/loginAcitvity/MainActivity.kt
+++ app/src/main/java/com/example/adsperclick/views/loginAcitvity/MainActivity.kt
@@ -1,17 +1,25 @@
-package com.example.adsperclick
+package com.example.adsperclick.views.loginAcitvity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adsperclick.R
+import com.example.adsperclick.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        //Start
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
